0e8a055910f15a94032a416fd9ee4c23dda690f0
Blagoja Tomanovski <b.tomanovski@nextech.com>
2023-05-18 01:18:51 +0200

    Add project files.

---
+++ API/Controllers/ResourcesController.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ResourcesController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public ResourcesController(ApplicationContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Resource>>> GetResources()
+        {
+            var resources = await _context.Resources.ToListAsync();
+
+            return resources;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Resource>> GetResource(int id)
+        {
+            var resource = await _context.Resources.FindAsync(id);
+
+            return resource;
+        }
+    }
+}
---
+++ Infrastructure/Data/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Resource> Resources { get; set; }
+    }
+}
